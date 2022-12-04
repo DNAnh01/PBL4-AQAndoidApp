f5cd3be4c110928019a9f0b85192a2b5b583722b
dnanh <donguyenanhgithub@gmail.com>
2022-12-04 10:30:19 +0700

    Đăng ký bằng cơ sở dữ liệu Firestore

--- PBL4-AQAndoidApp/app/src/main/java/com/dnanh/pbl4_aqandoidapp/activities/MainActivity.java
+++ PBL4-AQAndoidApp/app/src/main/java/com/dnanh/pbl4_aqandoidapp/activities/MainActivity.java
@@ -1,8 +1,10 @@
-package com.dnanh.pbl4_aqandoidapp;
+package com.dnanh.pbl4_aqandoidapp.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.dnanh.pbl4_aqandoidapp.R;
 
 public class MainActivity extends AppCompatActivity {
 
