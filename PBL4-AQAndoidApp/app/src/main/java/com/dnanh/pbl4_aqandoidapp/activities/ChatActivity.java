9634637494bc663e211aafd9c78d1ca6c8466e58
nhuquynh <honhuquynh2k2@gmail.com>
2022-12-06 00:51:42 +0700

    thiết kế bố cục màn hình trò chuyện

---
+++ PBL4-AQAndoidApp/app/src/main/java/com/dnanh/pbl4_aqandoidapp/activities/ChatActivity.java
@@ -0,0 +1,16 @@
+package com.dnanh.pbl4_aqandoidapp.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.dnanh.pbl4_aqandoidapp.R;
+
+public class ChatActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chat);
+    }
+}
